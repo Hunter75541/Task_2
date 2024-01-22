81ada77f923682b20e5b1cfe0973ab577c6f8ccf
Hunter75541 <129995445+Hunter75541@users.noreply.github.com>
2024-01-22 11:26:44 +0300

    Update hash.cpp

--- hash.cpp
+++ hash.cpp
@@ -2,24 +2,24 @@
 
 int CRH1::get_hash(std::string input) const
 {
-	std::cout << "This is crh1\n";
+	std::cout << "This is crh1" << std::endl;
 	return 1;
 }
 
 int CRH2::get_hash(std::string input) const
 {
-	std::cout << "This is crh2\n";
+	std::cout << "This is crh2" << std::endl;
 	return 2;
 }
 
 int SHA32::get_hash(std::string input) const
 {
-	std::cout << "This is sha32\n";
+	std::cout << "This is sha32" << std::endl;
 	return 3;
 }
 
 int SHA64::get_hash(std::string input) const
 {
-	std::cout << "This is sha64\n";
+	std::cout << "This is sha64" << std::endl;
 	return 4;
 }
